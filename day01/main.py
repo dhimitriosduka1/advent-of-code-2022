423b1cdd8d9c233e59ad633c0b7c4e0188e27fd1
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-12-03 16:40:07 +0100

    Fixed bug

--- day01/main.py
+++ day01/main.py
@@ -4,14 +4,20 @@ lines = file.readlines()
 max_calories_sum = 0
 current_calories_sum = 0
 
-for line in lines:
-    line = line[:len(line) - 1]
+
+def update_max():
+    global max_calories_sum
+    if current_calories_sum > max_calories_sum:
+        max_calories_sum = current_calories_sum
+
+
+for index, line in enumerate(lines):
+    line = line.removesuffix("\n")
     if len(line) == 0:
-        if current_calories_sum > max_calories_sum:
-            max_calories_sum = current_calories_sum
+        update_max()
         current_calories_sum = 0
     else:
         current_calories_sum += int(line)
+        update_max()
 
 print(max_calories_sum)
-
