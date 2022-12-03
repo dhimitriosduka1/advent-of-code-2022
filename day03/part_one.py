9ae3fc98751a78a532554ab31d3c3621ab8a265f
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-12-03 19:09:55 +0100

    Solved part one for day three

---
+++ day03/part_one.py
@@ -0,0 +1,27 @@
+file = open("./input/input.txt")
+lines = file.readlines()
+
+
+def get_priority(c: str) -> int:
+    lower_case_ascii_offset = 96
+    upper_case_ascii_offset = 38
+    return ord(c) - (upper_case_ascii_offset if c.isupper() else lower_case_ascii_offset)
+
+
+def get(data: str) -> str:
+    length = len(data)
+    middle = int(length / 2)
+
+    unique_chars_in_first_half = set(data[:middle])
+
+    for i in range(middle, length):
+        if unique_chars_in_first_half.__contains__(data[i]):
+            return data[i]
+
+
+score = 0
+
+for line in lines:
+    score += get_priority(get(line))
+
+print(score)
