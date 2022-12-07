36e3606ce9671e0940dea4d1b9f5a786f82d3ca7
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-12-07 21:10:54 +0100

    Refactor

--- day06/part_one.py
+++ day06/part_one.py
@@ -7,11 +7,12 @@ def has_unique_chars(string: str) -> bool:
 
 
 marker_index = 0
+marker_length = 4
 
-for i in range(len(data) - 3):
-    marker = data[i:i + 4]
+for i in range(len(data) - marker_length - 1):
+    marker = data[i:i + marker_length]
     if has_unique_chars(marker):
-        marker_index = i + 4
+        marker_index = i + marker_length
         break
 
 print(marker_index)
