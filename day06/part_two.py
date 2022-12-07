46b8ecf30091ef31377296fef9988639fb3f45c7
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-12-07 21:13:00 +0100

    Solved part two of day seven

---
+++ day06/part_two.py
@@ -0,0 +1,18 @@
+file = open("./input/input.txt")
+data = file.readline()
+
+
+def has_unique_chars(string: str) -> bool:
+    return len(set(string)) == len(string)
+
+
+marker_index = 0
+marker_length = 14
+
+for i in range(len(data) - marker_length - 1):
+    marker = data[i:i + marker_length]
+    if has_unique_chars(marker):
+        marker_index = i + marker_length
+        break
+
+print(marker_index)
